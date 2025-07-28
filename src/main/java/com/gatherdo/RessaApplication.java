98246e3ca707d0bd514a458f18e9c3a079fb6805
Peter <peterohwofasa@yahoo.com>
2025-07-28 16:27:24 +0200

    apllication setup

---
+++ src/main/java/com/gatherdo/RessaApplication.java
@@ -0,0 +1,13 @@
+package com.gatherdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RessaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gatherdo/RessaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gatherdo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessaApplicationTests {
+
+    @Test
+    @Disabled("Disabled until DB & context are configured properly")
+    void contextLoads() {
+    }
+
+}
